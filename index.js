4ba9b90ed5523e6f77c78f40c58275cbe10cbfa4
Victor Iglunov <116027529+WebArchitect89@users.noreply.github.com>
2023-11-23 11:11:16 +0000

    added the rest of JS code

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 console.log(randomNumber1);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+console.log(randomNumber2);
+
+var randomImage = " ./Dicee+Challenge+-+Starting+Files/images/dice"+randomNumber1+".png"
+console.log(randomImage)
+
+var randomImage2 = " ./Dicee+Challenge+-+Starting+Files/images/dice"+randomNumber2+".png"
+console.log(randomImage2)
+
+
+document.querySelector(".img1").setAttribute("src",randomImage);
+document.querySelector(".img2").setAttribute("src",randomImage2);
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = (" &#128681 Player 1 Wins!")
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = ("Player 2 Wins! &#128681")
+} else{
+    document.querySelector("h1").innerHTML = (" Draw ! ")
+}
